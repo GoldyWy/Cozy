c5dd5437b7e3ceef49ff28db5fba9f4d56724c89
GoldyWyy <goldy.widiyanto@bni.co.id>
2021-04-27 21:29:32 +0700

    Home page selesai design

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:cozy/models/city.dart';
 import 'package:cozy/models/space.dart';
+import 'package:cozy/models/tips.dart';
 import 'package:cozy/theme.dart';
+import 'package:cozy/widgets/bottom_navbar_item.dart';
 import 'package:cozy/widgets/city_card.dart';
 import 'package:cozy/widgets/space_card.dart';
+import 'package:cozy/widgets/tips_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -124,10 +127,76 @@ class HomePage extends StatelessWidget {
                       rating: 3))
                 ],
               ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            // NOTE : TIPS & GUIDANCE
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Tips & Guidance',
+                style: regulerTextStyle.copyWith(fontSize: 16),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Column(
+                children: [
+                  TipsCard(Tips(
+                      id: 1,
+                      title: 'City Guidelines',
+                      imageUrl: 'assets/images/tips1.png',
+                      updatedAt: '20 Apr')),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TipsCard(Tips(
+                      id: 1,
+                      title: 'Jakarta Fairship',
+                      imageUrl: 'assets/images/tips2.png',
+                      updatedAt: '11 Dec'))
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 60 + edge,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Container(
+        height: 65,
+        width: MediaQuery.of(context).size.width - (2 * edge),
+        margin: EdgeInsets.symmetric(horizontal: edge),
+        decoration: BoxDecoration(
+            color: Color(0xffF6F7F8), borderRadius: BorderRadius.circular(23)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            BottomNavbarItem(
+              imageUrl: 'assets/images/icon_home.png',
+              isActive: true,
+            ),
+            BottomNavbarItem(
+              imageUrl: 'assets/images/icon_email.png',
+              isActive: false,
+            ),
+            BottomNavbarItem(
+              imageUrl: 'assets/images/icon_card.png',
+              isActive: false,
+            ),
+            BottomNavbarItem(
+              imageUrl: 'assets/images/icon_love.png',
+              isActive: false,
             )
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
